2c3c6869530561154566657a9a3c98b72db4fa98
AZC <albertzcheng@gmail.com>
2021-12-07 22:36:54 -0800

    working MERN app with video paused at 37min

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    axios.get("/api/test").then(res => console.log(res));
+  }, [])
   return (
     <div className="App">
       <h1>Hello world</h1>
